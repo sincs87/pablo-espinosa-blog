4400efa119ec592eade422da9daf8a794024eee9
Pablo Espinosa Martínez <pespinosa@prensaiberica.es>
2025-11-26 08:31:36 +0100

    Fix astro.config.mjs with pabloespinosa.tech domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,31 @@
 import { defineConfig } from 'astro/config';
-syntaxHighlight: 'shiki',
-    shikiConfig: {
-    theme: 'github-dark-dimmed',
-        wrap: true,
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://pabloespinosa.tech',
+    output: 'hybrid',
+    adapter: cloudflare({
+        mode: 'directory',
+    }),
+    integrations: [
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'github-dark-dimmed',
+                wrap: true,
             },
         }),
-sitemap(),
+        sitemap(),
     ],
-vite: {
-    ssr: {
-        external: ['@supabase/supabase-js'],
+    vite: {
+        ssr: {
+            external: ['@supabase/supabase-js'],
         },
-},
-image: {
-    domains: ['supabase.co'],
+    },
+    image: {
+        domains: ['supabase.co'],
     },
 });
