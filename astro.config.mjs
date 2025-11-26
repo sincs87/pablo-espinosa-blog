3fd5d88ce88d99db3d968805efeb8c2d2d3cffab
Pablo Espinosa Martínez <pespinosa@prensaiberica.es>
2025-11-26 08:31:07 +0100

    Update site URL to pabloespinosa.tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,18 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://pabloespinosa.dev',
-    output: 'hybrid',
-    adapter: cloudflare({
-        mode: 'directory',
-    }),
-    integrations: [
-        mdx({
-            syntaxHighlight: 'shiki',
-            shikiConfig: {
-                theme: 'github-dark-dimmed',
-                wrap: true,
+syntaxHighlight: 'shiki',
+    shikiConfig: {
+    theme: 'github-dark-dimmed',
+        wrap: true,
             },
         }),
-        sitemap(),
+sitemap(),
     ],
-    vite: {
-        ssr: {
-            external: ['@supabase/supabase-js'],
+vite: {
+    ssr: {
+        external: ['@supabase/supabase-js'],
         },
-    },
-    image: {
-        domains: ['supabase.co'],
+},
+image: {
+    domains: ['supabase.co'],
     },
 });
